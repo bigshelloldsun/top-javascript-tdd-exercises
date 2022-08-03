c989fa00e333f5f36b5c2d4769fb1757f574e5d3
bigshelloldsun <bigshelloldsun@gmail.com>
2022-08-03 01:17:39 -0500

    solve exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let stringArray = Array.from(string);
+    let newString = [];
 
+    for (let i = string.length; i >= 0; i--) {
+        newString.push(stringArray[i]);
+    }
+    return newString.toString().replace(/,/g, '');
 };
 
 // Do not edit below this line
