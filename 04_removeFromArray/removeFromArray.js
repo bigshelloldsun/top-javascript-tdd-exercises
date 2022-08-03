cdb3a50ef1fd9f8a10190df0857d985b851985bc
bigshelloldsun <bigshelloldsun@gmail.com>
2022-08-03 01:27:41 -0500

    solve exercise 03

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, item) {
+    for (i of array) {
+        if (i === item) {
+            return array.splice(i, i + 1);
+        }
+    }
 };
 
 // Do not edit below this line
