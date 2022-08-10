e7cfc41cdd0f29250dcb612337bea3eb25729df3
bigshelloldsun <bigshelloldsun@gmail.com>
2022-08-10 02:51:03 -0500

    solve exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  let result = 0;
+  for (num of numbers) {
+    result += num;
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let result = 1;
+  for (num of numbers) {
+    result *= num;
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+  let result = 1;
+  for (let i = 0; i < y; i++) {
+    result *= x;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+  for (let i = num; i > 0; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
