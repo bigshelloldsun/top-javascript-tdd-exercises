48c1f7352db41f96c6ce247016982f9f8b84f76e
bigshelloldsun <bigshelloldsun@gmail.com>
2022-08-10 13:38:24 -0500

    solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+  if (number < 0) {
+    return 'OOPS';
+  }
+  
+  let sequence = [0,1];
 
+  for(let i = 0; i <= number; i++) {
+    if (i > 1) {
+      sequence.push(sequence[i - 1] + sequence[i - 2]);
+    }
+  }
+  return sequence[number];
 };
+
+//fibonacci(6);
 
 // Do not edit below this line
 module.exports = fibonacci;
