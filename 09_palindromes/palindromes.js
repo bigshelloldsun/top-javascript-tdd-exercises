8ff13efcc4c44c5dcea470501686cbd530c91695
bigshelloldsun <bigshelloldsun@gmail.com>
2022-08-10 03:48:58 -0500

    solve exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let strArray = Array.from(string.toLowerCase());
+  strArray = strArray.filter(item => {
+    if (item !== ',' && item !== '!' && item !== '.' && item !== ' ') {
+      return true;
+    }
+  });
+  let normal = strArray.toString().replace(/,/g,'');
+  let reversed = strArray.reverse().toString().replace(/,/g,'');
 
+  return normal === reversed ? true : false;
 };
 
 // Do not edit below this line
