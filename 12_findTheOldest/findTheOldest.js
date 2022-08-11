6a99b6204b361c225ba471a21d60e5dddf77a9e5
bigshelloldsun <bigshelloldsun@gmail.com>
2022-08-10 21:46:49 -0500

    refactor exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(list) {
+  const calcAge = (person) => {
+    const currentYear = new Date().getFullYear();
+    return !person.yearOfDeath ? currentYear - person.yearOfBirth : person.yearOfDeath - person.yearOfBirth;
+  };
 
+  return list.reduce(function(oldest, person) {
+    return calcAge(oldest) > calcAge(person) ? oldest : person;
+  });
 };
 
 // Do not edit below this line
